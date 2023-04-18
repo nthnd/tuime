9caef84b9a43553cf82f915328b33defebf25b4e
nathan dawit <nathandawit555@gmail.com>
2023-04-18 08:31:48 +0300

    feat: colors and fonts

--- src/args.rs
+++ src/args.rs
@@ -4,6 +4,18 @@ use clap::Parser;
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Format the time
-    #[arg(short, long, default_value_t = String::from("%H:%M"))]
+    #[arg(long, default_value_t = String::from("%H:%M"))]
     pub format: String,
+
+    /// Set Colors
+    #[arg(short, long)]
+    pub colors: Vec<String>,
+
+    /// Set Font
+    #[arg(short, long, default_value_t = String::from("Block"))]
+    pub font: String,
+
+    /// Set a gradient
+    #[arg(short, long)]
+    pub gradient: Vec<String>,
 }
--- src/display.rs
+++ src/display.rs
@@ -1,9 +1,51 @@
 use std::io::Write;
 
-use cfonts::{render, Align, Options};
+use cfonts::{render, Align, Colors, Fonts, Options};
 use crossterm::{cursor, style::Print, terminal, QueueableCommand};
 
 use crate::{args::Args, error::TuimeError};
+
+fn color_from_str(s: String) -> Colors {
+    match s.as_str() {
+        "Black" => Colors::Black,
+        "Red" => Colors::Red,
+        "Green" => Colors::Green,
+        "Yellow" => Colors::Yellow,
+        "Blue" => Colors::Blue,
+        "Magenta" => Colors::Magenta,
+        "Cyan" => Colors::Cyan,
+        "White" => Colors::White,
+        "Gray" => Colors::Gray,
+        "RedBright" => Colors::RedBright,
+        "GreenBright" => Colors::GreenBright,
+        "YellowBright" => Colors::YellowBright,
+        "BlueBright" => Colors::BlueBright,
+        "MagentaBright" => Colors::MagentaBright,
+        "CyanBright" => Colors::CyanBright,
+        "WhiteBright" => Colors::WhiteBright,
+        "Candy" => Colors::Candy,
+        _ => Colors::System,
+    }
+}
+
+fn font_from_str(s: &str) -> Fonts {
+    match s {
+        "Console" => Fonts::FontConsole,
+        "Block" => Fonts::FontBlock,
+        "SimpleBlock" => Fonts::FontSimpleBlock,
+        "Simple" => Fonts::FontSimple,
+        "3d" => Fonts::Font3d,
+        "Simple3d" => Fonts::FontSimple3d,
+        "Chrome" => Fonts::FontChrome,
+        "Huge" => Fonts::FontHuge,
+        "Shade" => Fonts::FontShade,
+        "Slick" => Fonts::FontSlick,
+        "Grid" => Fonts::FontGrid,
+        "Pallet" => Fonts::FontPallet,
+        "Tiny" => Fonts::FontTiny,
+        _ => Fonts::FontBlock,
+    }
+}
 
 pub fn get_time_str(args: &Args) -> String {
     let time = chrono::Local::now().format(&args.format).to_string();
@@ -11,6 +53,14 @@ pub fn get_time_str(args: &Args) -> String {
     let time_str = render(Options {
         text: time,
         align: Align::Center,
+        font: font_from_str(&args.font),
+        gradient: args.gradient.clone(),
+        colors: args
+            .colors
+            .clone()
+            .into_iter()
+            .map(color_from_str)
+            .collect::<Vec<Colors>>(),
         ..Options::default()
     });
 
@@ -18,7 +68,7 @@ pub fn get_time_str(args: &Args) -> String {
 }
 
 pub fn print_time(args: &Args) -> Result<(), TuimeError> {
-    let time_str: Vec<String> = get_time_str(&args)
+    let time_str: Vec<String> = get_time_str(args)
         .lines()
         .filter(|l| !l.is_empty())
         .map(str::to_string)
